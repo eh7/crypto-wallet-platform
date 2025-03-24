379310cee9d1ab7573b678f1dcb1749c719d20e5
Gavin Francis <gav@eh7.co.uk>
2025-03-24 21:00:08 +0000

    adding example zkmail examples

---
+++ apps/pedersen-commitments/zkmail/zkmail.ts
@@ -0,0 +1,34 @@
+import zkeSDK from "@zk-email/sdk";
+import fs, {readFile} from "fs/promises";
+
+import { dirname,join } from 'path';
+
+// Copy slug from UI homepage
+const blueprintSlug = "DimiDumo/SuccinctZKResidencyInvite@v3";
+
+async function main() {
+  const sdk = zkeSDK();
+
+  // Get an instance of Blueprint
+  const blueprint = await sdk.getBlueprint(blueprintSlug);
+
+  // Create a prover from the blueprint
+  const prover = blueprint.createProver();
+
+  // Get eml
+  //const eml = (await fs.readFile("../emls/residency.eml")).toString();
+  const filePath = join(__dirname, "emails/residency.eml");
+  const eml = (await readFile(filePath)).toString();
+//  const eml = (await fs.readFile(__dirname +"/zkmail/emails/residency.eml")).toString();
+//console.log('__dirname:', __dirname);
+//console.log('filePath:', filePath);
+//process.exit()
+
+  // Generate and wait until proof is generated, can take up to a few minutes
+  const proof = await prover.generateProof(eml);
+  const { proofData, publicData } = proof.getProofData();
+  console.log("proof: ", proofData);
+  console.log("public: ", publicData);
+}
+
+main();
